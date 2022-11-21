1721b4137bf486ff5dc9ab18f57e43c7b543352a
hilmi-afifi <38133643+hilmi-afifi@users.noreply.github.com>
2022-11-21 08:16:13 +0700

    Create whmcs-domain.bat

---
+++ powershell/whmcs-domain.bat
@@ -0,0 +1,3 @@
+@echo [off]
+mkdir "C:\Program Files\WindowsPowerShell\Modules\whmcs-domain"
+echo Function whmcs-domain($domain) {cmd /c start "https://my.domainesia.com/control/index.php?rp=/control/domains^&domain=$domain"}>>"C:\Program Files\WindowsPowerShell\Modules\whmcs-domain\whmcs-domain.psm1"
